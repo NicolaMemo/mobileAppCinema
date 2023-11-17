292be228f00b3dfb159e152bf47b05323f0342fb
Ettore Canever <etto06etto@gmail.com>
2023-11-17 13:17:51 +0100

    vamos cinema don bosco api

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import Film1 from './pages/Film1'
 import Film2 from './pages/Film2'
 import Film3 from './pages/Film3'
 import Compra from './pages/Compra';
+import ActualFilm from './pages/ActualFilm';
 const Stack = createNativeStackNavigator()
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
         <Stack.Screen name='Film2' component={ Film2 } options={{ title: 'Film 2'}} />
         <Stack.Screen name='Film3' component={ Film3 } options={{ title: 'Film 3'}} />
         <Stack.Screen name='Compra' component={ Compra } options={{ title: 'Compra'}} />
+        <Stack.Screen name='ActualFilm' component={ ActualFilm } options={{ title: 'ActualFilm'}} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- pages/Home.js
+++ pages/Home.js
@@ -6,36 +6,69 @@ import {
   StyleSheet,
   ScrollView,
   Pressable,
+  ActivityIndicator,
+  ImageBackground,
+  FlatList,
 } from "react-native";
-
+import React, { useEffect, useState } from "react";
 export default function Home({ navigation }) {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMoviesFromApi = async () => {
+    try {
+      const response = await fetch(
+        "https://cinema.donboscosandona.it/movie/featured.json"
+      );
+      const json = await response.json();
+      console.log(json);
+      setMovies(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getMoviesFromApi();
+  }, []);
   return (
     <View style={styles.container}>
       <ScrollView>
-        <Pressable onPress={() => navigation.navigate("Film1")}>
-          <Image
-            style={styles.logo}
-            source={{
-              uri: "https://image.tmdb.org/t/p/original/fLiEU12qHCs2xP73OgLFVVObS1k.png",
-            }}
-          />
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate("Film2")}> 
-          <Image
-            style={styles.logo}
-            source={{
-              uri: "https://image.tmdb.org/t/p/original/8aShJDCnsAB2fTsJR92yuaLvPeu.png",
-            }}
-          />
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate("Film3")}>
-          <Image
-            style={styles.logo}
-            source={{
-              uri: "https://image.tmdb.org/t/p/original/tAVA9Wg11RFwOIkQ3Hn8KMk4Xw3.png",
-            }}
-          />
-        </Pressable>
+        <ImageBackground resizeMode="cover">
+          {isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <FlatList
+              data={movies}
+              keyExtractor={({ id }) => id}
+              renderItem={({ item }) => (
+                <>
+                  <Image
+                    style={styles.image}
+                    source={{ uri: item.poster_path }}
+                  />
+                  <Button
+                    title={
+                      item.title +
+                      "\n di: " +
+                      item.director +
+                      "\n DA:" +
+                      item.showtimes[0].date +
+                      "\n Alle: " +
+                      item.showtimes[0].time
+                    }
+                    onPress={() => {
+                      /* 1. Navigate to the Details route with params */
+                      navigation.navigate("ActualFilm", {
+                        id: item.id,
+                      });
+                    }}
+                  />
+                </>
+              )}
+            />
+          )}
+        </ImageBackground>
       </ScrollView>
     </View>
   );
@@ -45,10 +78,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 2,
-    margin:10,
+    margin: 10,
   },
   logo: {
     height: 150,
-    resizeMode: 'contain',
+    resizeMode: "contain",
+  },
+  spazio: {
+    padding: 10,
+  },
+  image: {
+    height: 390,
   },
 });
